bb8889b1e28f39690b0f8d9ddc4b7f346b52c587
woodenToaster <litmusik@yahoo.com>
2014-12-09 13:32:10 -0600

    Ready to present

--- navbar.php
+++ navbar.php
@@ -2,18 +2,20 @@
   <div class="container-fluid">
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
+      <h4><span class="glyphicon glyphicon-music"></span><b>RandoMIDI</b></h4>
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
         <span class="sr-only">Toggle navigation</span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <p class="navbar-brand"><span class="glyphicon glyphicon-music"></span>RandoMIDI</p>
+      
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
+        
         <li><a href="songs.php"><span class="glyphicon glyphicon-play"></span>Songs <span class="sr-only">(current)</span></a></li>
         <li><a href="index.php"><span class="glyphicon glyphicon-pencil"></span>Create <span class="sr-only">(current)</span></a></li>
       </ul>
