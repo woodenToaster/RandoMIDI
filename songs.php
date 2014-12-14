9944c0a669a1cf9c974983b6f23874be48e6858f
Wesley Hoffman <wesleyhoffman109@gmail.com>
2014-12-14 17:09:28 -0600

    ...

--- songs.php
+++ songs.php
@@ -57,7 +57,6 @@
     </div>
   </body>
 </html>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <script>
 $(document).ready(function(){
     $('.button').click(function(){
