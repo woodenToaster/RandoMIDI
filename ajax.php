7007f55f9f015e02f89064f0128eba8bf7a611d6
Wesley Hoffman <wesleyhoffman109@gmail.com>
2014-12-14 17:13:36 -0600

    ...

--- ajax.php
+++ ajax.php
@@ -12,6 +12,6 @@ if (isset($_POST['action'])) {
 function play(){
     require('midi_class_v178/classes/midi.class.php');
     $midi = new Midi();
-    $midi->playMidFile(taylor_swift-shake_it_off.mid,0,1,0,'quicktime');
+    $midi->playMidFile('taylor_swift-shake_it_off.mid',0,1,0,'quicktime');
 }
 ?>
